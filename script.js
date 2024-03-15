676dc0524f8ffa47b2dde3778453c9ca11e61394
Hamza Shaukat <75229817+hamzashaukat111@users.noreply.github.com>
2024-03-16 00:55:06 +0500

    integrated liveness api

--- script.js
+++ script.js
@@ -32,14 +32,76 @@ $(document).ready(function () {
 
   function processImage(formData) {
     $.ajax({
-      url: "https://cvwatermark-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/734afe81-8fa0-45d9-8bc5-81d075b50746/classify/iterations/Iteration1/image",
+      url: "https://cvscanfood-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/6ed5e91e-7c3c-44db-bcba-651a7bbb76f6/classify/iterations/Iteration1/image",
       type: "POST",
       data: formData,
       processData: false,
       contentType: false,
       headers: {
-        "Prediction-Key": "5e19fdcca31a48878e82eb0b7b226244",
+        "Prediction-Key": "6d9918f13de44cdbb32081baaf81387c",
       },
+      //   success: function (response) {
+      //     var predictions = response.predictions;
+      //     var resultContainer = document.getElementById("resultContainer");
+      //     var resultHeading = document.getElementById("resultHeading");
+      //     resultContainer.innerHTML = "";
+      //     resultHeading.innerHTML = "";
+
+      //     if (predictions.length > 0) {
+      //       var watermarkProbability = 0;
+      //       var noWatermarkProbability = 0;
+      //       for (var i = 0; i < predictions.length; i++) {
+      //         var prediction = predictions[i];
+      //         if (prediction.tagName === "Watermark") {
+      //           watermarkProbability = prediction.probability;
+      //         } else if (prediction.tagName === "NoWatermark") {
+      //           noWatermarkProbability = prediction.probability;
+      //         }
+      //       }
+
+      //       var result = '<div class="result-container">';
+      //       result += '<div class="prediction">';
+      //       result += '<p class="tag-name">Watermark</p>';
+      //       result += '<div class="percentage-bar">';
+      //       result +=
+      //         '<div class="percentage" style="width: ' +
+      //         watermarkProbability * 100 +
+      //         '%;"></div>';
+      //       result += "</div>";
+      //       result +=
+      //         '<p class="probability">' +
+      //         (watermarkProbability * 100).toFixed(2) +
+      //         "%</p>";
+      //       result += "</div>";
+
+      //       result += '<div class="prediction">';
+      //       result += '<p class="tag-name">No Watermark</p>';
+      //       result += '<div class="percentage-bar">';
+      //       result +=
+      //         '<div class="percentage" style="width: ' +
+      //         noWatermarkProbability * 100 +
+      //         '%;"></div>';
+      //       result += "</div>";
+      //       result +=
+      //         '<p class="probability">' +
+      //         (noWatermarkProbability * 100).toFixed(2) +
+      //         "%</p>";
+      //       result += "</div>";
+      //       result += "</div>";
+
+      //       resultContainer.innerHTML = result;
+
+      //       if (watermarkProbability > noWatermarkProbability) {
+      //         resultHeading.innerHTML =
+      //           '<h2 class="result-heading">This Video contains a watermark</h2>';
+      //       } else {
+      //         resultHeading.innerHTML =
+      //           '<h2 class="result-heading">This Video does not contain a watermark</h2>';
+      //       }
+      //     } else {
+      //       resultContainer.innerHTML = "<p>No predictions found.</p>";
+      //     }
+      //   },
       success: function (response) {
         var predictions = response.predictions;
         var resultContainer = document.getElementById("resultContainer");
@@ -48,60 +110,61 @@ $(document).ready(function () {
         resultHeading.innerHTML = "";
 
         if (predictions.length > 0) {
-          var watermarkProbability = 0;
-          var noWatermarkProbability = 0;
+          var aliveProbability = 0;
+          var notAliveProbability = 0;
           for (var i = 0; i < predictions.length; i++) {
             var prediction = predictions[i];
-            if (prediction.tagName === "Watermark") {
-              watermarkProbability = prediction.probability;
-            } else if (prediction.tagName === "NoWatermark") {
-              noWatermarkProbability = prediction.probability;
+            if (prediction.tagName === "Alive") {
+              aliveProbability = prediction.probability;
+            } else if (prediction.tagName === "notalive") {
+              notAliveProbability = prediction.probability;
             }
           }
 
           var result = '<div class="result-container">';
           result += '<div class="prediction">';
-          result += '<p class="tag-name">Watermark</p>';
+          result += '<p class="tag-name">Alive</p>';
           result += '<div class="percentage-bar">';
           result +=
             '<div class="percentage" style="width: ' +
-            watermarkProbability * 100 +
+            aliveProbability * 100 +
             '%;"></div>';
           result += "</div>";
           result +=
             '<p class="probability">' +
-            (watermarkProbability * 100).toFixed(2) +
+            (aliveProbability * 100).toFixed(2) +
             "%</p>";
           result += "</div>";
 
           result += '<div class="prediction">';
-          result += '<p class="tag-name">No Watermark</p>';
+          result += '<p class="tag-name">Not Alive</p>';
           result += '<div class="percentage-bar">';
           result +=
             '<div class="percentage" style="width: ' +
-            noWatermarkProbability * 100 +
+            notAliveProbability * 100 +
             '%;"></div>';
           result += "</div>";
           result +=
             '<p class="probability">' +
-            (noWatermarkProbability * 100).toFixed(2) +
+            (notAliveProbability * 100).toFixed(2) +
             "%</p>";
           result += "</div>";
           result += "</div>";
 
           resultContainer.innerHTML = result;
 
-          if (watermarkProbability > noWatermarkProbability) {
+          if (aliveProbability > notAliveProbability) {
             resultHeading.innerHTML =
-              '<h2 class="result-heading">This Video contains a watermark</h2>';
+              '<h2 class="result-heading">This Video is Alive</h2>';
           } else {
             resultHeading.innerHTML =
-              '<h2 class="result-heading">This Video does not contain a watermark</h2>';
+              '<h2 class="result-heading">This Video is Not Alive</h2>';
           }
         } else {
           resultContainer.innerHTML = "<p>No predictions found.</p>";
         }
       },
+
       error: function () {
         var resultContainer = document.getElementById("resultContainer");
         var resultHeading = document.getElementById("resultHeading");
